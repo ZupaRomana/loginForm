04fdb842c5875172bcd93c229013c532424cdb26
zuparomana <adam.romanowski93@gmail.com>
2018-08-29 11:24:30 +0200

    Init commit

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -1,13 +1,27 @@
 package com.codecool;
 
-/**
- * Hello world!
- *
- */
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        HttpServer httpServer = null;
+        try {
+            httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        httpServer.createContext("/index", new AccountController());
+        httpServer.createContext("/static", new Static());
+
+        httpServer.setExecutor(null);
+
+        httpServer.start();
+
     }
 }
