5d079163f0b3074711c5bc131bb360ad67158e5a
zuparomana <adam.romanowski93@gmail.com>
2018-08-29 12:55:49 +0200

    Implement getting data entered by user

--- src/main/java/com/codecool/AccountController.java
+++ src/main/java/com/codecool/AccountController.java
@@ -5,8 +5,10 @@ import com.sun.net.httpserver.HttpHandler;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AccountController implements HttpHandler {
 
@@ -29,8 +31,33 @@ public class AccountController implements HttpHandler {
         sendResponse();
     }
 
-    private void handleData() {
+    private void handleData() throws IOException {
 
+        String formData = getFormData();
+        Map<String, String> inputs = parseFormData(formData);
+        String login = inputs.get("login");
+        String password = inputs.get("password");
+
+        System.out.println(login);
+        System.out.println(password);
+    }
+
+    private String getFormData() throws IOException {
+        InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+        BufferedReader br = new BufferedReader(isr);
+
+        return br.readLine();
+    }
+
+    private static Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
+        Map<String, String> map = new HashMap<>();
+        String[] pairs = formData.split("&");
+        for(String pair : pairs){
+            String[] keyValue = pair.split("=");
+            String value = new URLDecoder().decode(keyValue[1], "UTF-8");
+            map.put(keyValue[0], value);
+        }
+        return map;
     }
 
     private void prepareStaticHtml() {
