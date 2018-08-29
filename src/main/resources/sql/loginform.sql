dbd76fa34da9cfee05a686851fe2e649c2a8013b
zuparomana <adam.romanowski93@gmail.com>
2018-08-29 11:18:23 +0200

    Create table query with insert some data

---
+++ src/main/resources/sql/loginform.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE user_data
+(login text,
+password text);
+
+INSERT INTO user_data
+VALUES ('admin', 'admin');
+
+INSERT INTO user_data
+VALUES ('roman', 'qwerty');
+
+INSERT INTO user_data
+VALUES ('mikolajry', '12345');
